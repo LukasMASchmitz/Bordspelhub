6115849519b0c7c3714d767cd6ceeb39c679bbdf
Lukas <91724945+LukasMASchmitz@users.noreply.github.com>
2022-12-08 13:33:20 +0100

    Identity

--- Bordspelhub/Program.cs
+++ Bordspelhub/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Bordspelhub.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BordspelhubContextConnection") ?? throw new InvalidOperationException("Connection string 'BordspelhubContextConnection' not found.");
+
+builder.Services.AddDbContext<BordspelhubContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BordspelhubContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,5 +31,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
